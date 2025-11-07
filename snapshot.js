7ee69e7f99280f6defa97f860ee5d7ee2a80b8b4
atharvabulusu-creator <atharvabulusu@gmail.com>
2025-11-06 20:13:37 -0500

    snapshot.js

---
+++ snapshot.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  // Launch browser
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  // Go to site
+  await page.goto('https://ducklit.com', { waitUntil: 'networkidle2' });
+
+  // Save rendered HTML snapshot
+  const html = await page.content();
+  fs.writeFileSync('ducklit_snapshot.html', html);
+
+  // Save screenshot (optional)
+  await page.screenshot({ path: 'ducklit.png', fullPage: true });
+
+  await browser.close();
+})();
